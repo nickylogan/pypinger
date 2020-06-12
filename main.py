5f45afbf712e83fcbcd71fb7977c6d0543ccd9e3
Nicky Logan <laurentiusdominick@yahoo.com>
2020-06-12 12:44:35 +0700

    chore: remove unused libraries and style cleanup

--- main.py
+++ main.py
@@ -4,60 +4,35 @@ import datetime as dt
 import matplotlib as mpl
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-from matplotlib.figure import Axes
-from matplotlib.ticker import MaxNLocator
 from pythonping import ping
 
-# disables default toolbar. Uncomment this line if you wanna show them.
+# disables default toolbar
+# comment out this line if you wanna show them.
 mpl.rcParams['toolbar'] = 'None'
 
 
 class Pinger:
-    """A class for pinging hosts"""
-
     TIMEOUT = 2000  # default timeout (in ms)
 
     def __init__(self, host: str, timeout: int = TIMEOUT):
-        """Creates a new Pinger
-
-        Arguments:
-            host {str} -- the host to ping
-            timeout {int} -- the timeout (in ms)
-        """
         self.host = host
         self.timeout = timeout
 
     def call(self) -> float:
-        """Invokes a ping command
-
-        Returns:
-            float -- the round trip time (in ms)
-        """
         try:
             resp = ping(self.host, count=1, timeout=self.timeout/1000)
             rtt = resp.rtt_avg_ms
         except Exception as e:
-            rtt = Pinger.TIMEOUT
+            rtt = self.timeout
 
         return rtt
 
 
 class PingPlotter:
-    """A class for plotting pings"""
-
     LIMIT = 1000  # default limit of data points to display
     INTERVAL = 500  # default interval between pings (in ms)
 
     def __init__(self, pinger: Pinger, limit: int = LIMIT, interval: int = INTERVAL):
-        """Creates a new PingPlotter
-
-        Arguments:
-            pinger {Pinger} -- a Pinger instance to invoke a ping request.
-
-        Keyword Arguments:
-            limit {int} -- maximum number of data points to display (default: {LIMIT})
-            interval {int} -- time interval (in ms) between consecutive ping calls  (default: {INTERVAL})
-        """
         self.pinger = pinger
         self.limit = limit
         self.interval = interval
@@ -66,48 +41,37 @@ class PingPlotter:
         self.timestamps = []
         self.rtts = []
 
+        # init plot
+        self.fig, self.ax = plt.subplots()
+
+    def __update_data(self):
+        rtt = self.pinger.call()
+
+        self.timestamps.append(dt.datetime.now())
+        self.timestamps = self.timestamps[-self.limit:]
+        self.rtts.append(rtt)
+        self.rtts = self.rtts[-self.limit:]
+
+    def __render_frame(self, i: int):
+        self.__update_data()
+
+        self.ax.clear()
+        self.ax.grid(True)
+        self.ax.plot_date(self.timestamps, self.rtts, 'b-')
+
+        host = self.pinger.host
+        plt.title('Latency over time to {}'.format(host))
+        plt.ylabel('Round-trip time (ms)')
+
     def start(self):
-        """Starts the plotter to show pings"""
-
-        fig = plt.figure()
-        ax = fig.add_subplot()
-
-        def __render_frame(i: int):
-            """Renders the current frame"""
-
-            rtt = self.pinger.call()
-
-            self.timestamps.append(dt.datetime.now())
-            self.rtts.append(rtt)
-
-            # Truncate data points to limit
-            self.timestamps = self.timestamps[-self.limit:]
-            self.rtts = self.rtts[-self.limit:]
-
-            ax.clear()
-            ax.grid(True)
-            ax.plot_date(self.timestamps, self.rtts, 'b-')
-
-            # Show labels
-            host = self.pinger.host
-            plt.title('Latency over time to {}'.format(host))
-            plt.ylabel('Round-trip time (ms)')
-
-        # Periodically update the graph
+        # assign to variable to avoid garbage collection.
         a = animation.FuncAnimation(
-            fig=fig,
-            func=__render_frame,
+            fig=self.fig,
+            func=self.__render_frame,
             interval=self.interval,
         )
 
         plt.show()
-
-    def stop(self):
-        """Stops the plotter"""
-
-        plt.close()
-        self.timestamps = []
-        self.rtts = []
 
 
 if __name__ == "__main__":
